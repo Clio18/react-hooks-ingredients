28f4f608b46429fb0b2b27a4bcceb01229570d37
U0L9511 <estclio09@gmail.com>
2022-07-12 17:27:17 +0300

    Error handling for remove was added

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,10 +3,12 @@ import React, { useState, useCallback } from "react";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from "../UI/ErrorModal";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const addIngredientHandler = (ingredient) => {
     setIsLoading(true);
@@ -39,12 +41,16 @@ const Ingredients = () => {
       {
         method: "DELETE",
       }
-    ).then((response) => {
-      setIsLoading(false);
-      setUserIngredients((prevIngredients) =>
-        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-      );
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        setUserIngredients((prevIngredients) =>
+          prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        );
+      })
+      .catch((error) => {
+        setError("Something goes wrong!");
+      });
   };
 
   // 2. all time when state changed (when ingredients are changed) this function will be re-render
@@ -56,8 +62,14 @@ const Ingredients = () => {
     [setUserIngredients]
   );
 
+  const clearError = () => {
+    setError(null);
+    isLoading(false);
+  };
+
   return (
     <div className="App">
+      {error ? <ErrorModal onClose={clearError}>{error}</ErrorModal> : null}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading}
