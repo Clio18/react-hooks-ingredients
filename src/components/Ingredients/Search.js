ff1803b0e8f93364c0ba55f82b90260f8001ba14
U0L9511 <estclio09@gmail.com>
2022-07-12 16:06:17 +0300

    useRef: set timer to check when user stop typing, use cleanup function to reset the timer

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,37 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo((props) => {
-  // 1. retreive from props only onLoadedIngredients, and after we can set it to the array
-  // of depemdencies in useEffect, so useEffect wii run only when onLoadedIngredients changed
   const { onLoadedIngredients } = props;
   const [enteredFilter, setenteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-    fetch(
-      "https://update-react-ingredients-default-rtdb.firebaseio.com/ingredients.json" +
-        query
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+        fetch(
+          "https://update-react-ingredients-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadedIngredients(loadedIngredients);
           });
-        }
-        onLoadedIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadedIngredients]);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadedIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -39,6 +45,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => {
